618f04a151ae4feb38142f7109561d497e0d3cc8
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2024-03-06 16:45:42 -0300

    Microdesafio 1 completado

--- src/components/ContentRowMovies.jsx
+++ src/components/ContentRowMovies.jsx
@@ -1,67 +1,19 @@
 import React from 'react'
+import { Widget } from './Widget'
 
 export const ContentRowMovies = () => {
   return (
     <>
       <div className='row'>
         <div className='col-md-4 mb-4'>
-          <div className='card border-left-primary shadow h-100 py-2'>
-            <div className='card-body'>
-              <div className='row no-gutters align-items-center'>
-                <div className='col mr-2'>
-                  <div className='text-xs font-weight-bold text-primary text-uppercase mb-1'>
-                    Movies in Data Base
-                  </div>
-                  <div className='h5 mb-0 font-weight-bold text-gray-800'>
-                    21
-                  </div>
-                </div>
-                <div className='col-auto'>
-                  <i className='fas fa-film fa-2x text-gray-300'></i>
-                </div>
-              </div>
-            </div>
-          </div>
+            <Widget titulo="movies in data base" cifra={27} colorBorde='primary' icono='fa-film'/>
         </div>
 
         <div className='col-md-4 mb-4'>
-          <div className='card border-left-success shadow h-100 py-2'>
-            <div className='card-body'>
-              <div className='row no-gutters align-items-center'>
-                <div className='col mr-2'>
-                  <div className='text-xs font-weight-bold text-success text-uppercase mb-1'>
-                    {' '}
-                    Total awards
-                  </div>
-                  <div className='h5 mb-0 font-weight-bold text-gray-800'>
-                    79
-                  </div>
-                </div>
-                <div className='col-auto'>
-                  <i className='fas fa-award fa-2x text-gray-300'></i>
-                </div>
-              </div>
-            </div>
-          </div>
+            <Widget titulo="total awards" cifra={79} colorBorde='success' icono='fa-award'/>
         </div>
         <div className='col-md-4 mb-4'>
-          <div className='card border-left-warning shadow h-100 py-2'>
-            <div className='card-body'>
-              <div className='row no-gutters align-items-center'>
-                <div className='col mr-2'>
-                  <div className='text-xs font-weight-bold text-warning text-uppercase mb-1'>
-                    Actors quantity
-                  </div>
-                  <div className='h5 mb-0 font-weight-bold text-gray-800'>
-                    49
-                  </div>
-                </div>
-                <div className='col-auto'>
-                  <i className='fas fa-user fa-2x text-gray-300'></i>
-                </div>
-              </div>
-            </div>
-          </div>
+            <Widget titulo="actors quantity" cifra={49} colorBorde="warning" icono='fa-user'/>
         </div>
       </div>
     </>
